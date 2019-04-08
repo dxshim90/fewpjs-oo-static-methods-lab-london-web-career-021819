23e734e430c32606069cb84998079d792e2d0ed7
github username <github email address>
2019-04-08 12:13:02 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+ return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    string = string.replace(/[^a-z0-9áéíóúñü '\.,_-]/gim,"");
+    return string.trim();
+  }
+
+  static titleize(string) {
+    var words = string.match(/\b\w+\b/g),
+      preps = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+  return words.map(function(e,i) {
+                     return preps.indexOf(e) == -1 || i === 0 ? e[0].toUpperCase()+e.slice(1) : e;
+                   }).join(" ");
+  }
+
 }
